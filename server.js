14ec71884e08ca837070997808a8f7227fe5dabd
MrMendonez <dmendonez@gmail.com>
2016-02-22 12:56:11 -0500

    init commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var PORT = process.env.NODE_ENV || 3000;
 
 // database setup
 var Sequelize = require('sequelize');
-var connection = new Sequelize('secret_clubhouse_db', 'root');
+var connection = new Sequelize('class_app_db', 'root');
 
 //requiring passport last
 var passport = require('passport');
@@ -19,3 +19,107 @@ app.use(require('express-session')({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+
+//passport use methed as callback when being authenticated
+passport.use(new passportLocal.Strategy(function(username, password, done) {
+    //check password in db
+    User.findOne({
+        where: {
+            username: username
+        }
+    }).then(function(user) {
+        //check password against hash
+        if(user){
+            bcrypt.compare(password, user.dataValues.password, function(err, user) {
+                if (user) {
+                  //if password is correct authenticate the user with cookie
+                  done(null, { id: username, username: username });
+                } else{
+                  done(null, null);
+                }
+            });
+        } else {
+            done(null, null);
+        }
+    });
+
+}));
+
+//change the object used to authenticate to a smaller token, and protects the server from attacks
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+passport.deserializeUser(function(id, done) {
+    done(null, { id: id, username: id })
+});
+
+var bcrypt = require('bcryptjs');
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+var User = connection.define('user', {
+  username: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      len: {
+        args: [5,20],
+        msg: "Your password must be between 5-20 characters"
+      },
+    }
+  }
+}, {
+  hooks: {
+    beforeCreate: function(input){
+      input.password = bcrypt.hashSync(input.password, 10);
+    }
+  }
+});
+
+
+//handlebars setup
+var expressHandlebars = require('express-handlebars');
+app.engine('handlebars', expressHandlebars({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+//check login with db
+app.post('/check', passport.authenticate('local', {
+    successRedirect: '/home',
+    failureRedirect: '/?msg=Login Credentials do not work'
+}));
+
+
+app.get("/", function(req, res){
+  res.render('index', {msg: req.query.msg});
+})
+
+app.get('/home', function(req, res){
+  res.render('home', {
+    user: req.user,
+    isAuthenticated: req.isAuthenticated()
+  });
+})
+app.post("/save", function(req, res){
+  User.create(req.body).then(function(result){
+    res.redirect('/?msg=Account created');
+  }).catch(function(err) {
+    console.log(err);
+    res.redirect('/?msg=' + err.errors[0].message);
+  });
+})
+
+// database connection via sequelize
+connection.sync().then(function() {
+  app.listen(PORT, function() {
+      console.log("Listening on:" + PORT)
+  });
+});
